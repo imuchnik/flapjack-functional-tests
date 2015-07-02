bb24abc84218b4cbc1f4c9919cd1284b3a12b2cd
Irina Muchnik <irina.muchnik@cfpb.gov>
2015-07-02 10:55:43 -0400

    adding sauce

--- conf.js
+++ conf.js
@@ -3,7 +3,8 @@ var env = require('./env.js');
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   capabilities: {'browserName': 'chrome'},
-
+  sauceUser: "davisd",
+  sauceKey:'ee6e40da-42dd-45cf-b622-e16444393f3c',
   //suites: {
   //  homepage: 'tests/e2e/homepage/**/*Spec.js',
   //  search: ['tests/e2e/contact_search/**/*Spec.js']
