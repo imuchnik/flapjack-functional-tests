7d097e912bf8967b66061eb8628d6ec7baafd9d8
Irina Muchnik <irina.muchnik@cfpb.gov>
2015-07-06 14:51:37 -0400

    adding flapjack sauce credentials

--- conf.js
+++ conf.js
@@ -1,39 +1,50 @@
 var env = require('./env.js');
 
+
 exports.config = {
   //seleniumAddress: 'http://localhost:4444/wd/hub',
   capabilities: {'browserName': 'chrome'},
-  sauceUser: "davisd",
-  sauceKey:'ee6e40da-42dd-45cf-b622-e16444393f3c',
-  //suites: {
-  //  homepage: 'tests/e2e/homepage/**/*Spec.js',
-  //  search: ['tests/e2e/contact_search/**/*Spec.js']
-  //},
 
-  // This can be changed via the command line as:
-  // --params.login.user 'ngrocks'
-  onPrepare: function() {
+sauceUser: "flapjack",
+sauceKey:'2f025b43-99f8-4674-9570-ee5319147cae',
+////suites: {
+//  homepage: 'tests/e2e/homepage/**/*Spec.js',
+//  search: ['tests/e2e/contact_search/**/*Spec.js']
+//},
 
+// This can be changed via the command line as:
+// --params.login.user 'ngrocks'
+  onPrepare
+:
+function() {
 
-    //var folderName = (new Date()).toString().split(' ').splice(1, 4).join(' ');
-    //var mkdirp = require('mkdirp');
-    //var newFolder = "./reports/" + folderName;
-    //require('jasmine-reporters');
-    //
-    //mkdirp(newFolder, function(err) {
-    //  if (err) {
-    //    console.error(err);
-    //  } else {
-    //    jasmine.getEnv().addReporter(new jasmine.JUnitXmlReporter(newFolder, true, true));
-    //  }
-    //});
-    //jasmine.getEnv().addReporter(new jasmine.JUnitXmlReporter('xmloutput', true, true));
-    browser.driver.manage().window().setSize(1600, 800);
-    browser.ignoreSynchronization = true;
-  },
+  //
+  //var folderName = (new Date()).toString().split(' ').splice(1, 4).join(' ');
+  //var mkdirp = require('mkdirp');
+  //var newFolder = "./reports/" + folderName;
+  //
+  //require('jasmine-reporters');
+  //
+  //mkdirp(newFolder, function(err) {
+  //  if (err) {
+  //    console.error(err);
+  //  } else {
+  //    console.log( jasmine.JUnitXmlReporter);
+  //    jasmine
+  //      .getEnv()
+  //      .addReporter(
+  //      new jasmine.JUnitXmlReporter(newFolder, true, true));
+  //  }
+  //});
+  //jasmine.getEnv().addReporter(new jasmine.JUnitXmlReporter('xmloutput', true, true));
+  browser.driver.manage().window().setSize(1600, 800);
+  browser.ignoreSynchronization = true;
+}
+,
 
-  specs: ['FJ-about-smoke-spec.js']
-};
+specs: ['FJ-about-smoke-spec.js']
+}
+;
 //protractor protractor.conf.js --suite homepage
 //npm install jasmine-reporters
 //npm install mkdirp
