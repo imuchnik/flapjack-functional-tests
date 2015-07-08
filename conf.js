4bf6e0390e468871106e79e53293089b756e6f4c
Irina Muchnik <irina.muchnik@cfpb.gov>
2015-07-08 16:48:21 -0400

    adding reports

--- conf.js
+++ conf.js
@@ -2,47 +2,49 @@ var env = require('./env.js');
 
 
 exports.config = {
-  //seleniumAddress: 'http://localhost:4444/wd/hub',
+  framework: 'jasmine2',
+ // seleniumAddress: 'http://localhost:4444/wd/hub',
   capabilities: {'browserName': 'chrome'},
 
+
+  specs: ['FJ-about-smoke-spec.js'],
+//
 sauceUser: "flapjack",
 sauceKey:'2f025b43-99f8-4674-9570-ee5319147cae',
-////suites: {
+//////suites: {
 //  homepage: 'tests/e2e/homepage/**/*Spec.js',
 //  search: ['tests/e2e/contact_search/**/*Spec.js']
 //},
 
-// This can be changed via the command line as:
-// --params.login.user 'ngrocks'
-  onPrepare
-:
-function() {
+  onPrepare: function() {
+    var jasmineReporters = require('jasmine-reporters');
+    var mkdirp = require('mkdirp');
+    var SpecReporter = require('jasmine-spec-reporter');
+    // add jasmine spec reporter
+    jasmine.getEnv().addReporter(new SpecReporter({displayStacktrace: true}));
 
-  //
-  //var folderName = (new Date()).toString().split(' ').splice(1, 4).join(' ');
-  //var mkdirp = require('mkdirp');
-  //var newFolder = "./reports/" + folderName;
-  //
-  //require('jasmine-reporters');
-  //
-  //mkdirp(newFolder, function(err) {
-  //  if (err) {
-  //    console.error(err);
-  //  } else {
-  //    console.log( jasmine.JUnitXmlReporter);
-  //    jasmine
-  //      .getEnv()
-  //      .addReporter(
-  //      new jasmine.JUnitXmlReporter(newFolder, true, true));
-  //  }
-  //});
-  //jasmine.getEnv().addReporter(new jasmine.JUnitXmlReporter('xmloutput', true, true));
-  browser.driver.manage().window().setSize(1600, 800);
-  browser.ignoreSynchronization = true;
-}
-,
+    browser.driver.manage().window().setSize(1600, 800);
+    browser.ignoreSynchronization = true;
 
-specs: ['FJ-about-smoke-spec.js']
+    var folderName = (new Date()).toString().split(' ').splice(1, 4).join('-');
+    var newFolder = "reports/" + folderName;
+
+    mkdirp(newFolder, function( err ) {
+        if (err) {
+          console.error(err);
+        } else {
+          var jUnitXmlReporter = new jasmineReporters.JUnitXmlReporter(
+            {consolidateAll: true,
+              savePath: newFolder,
+              filePrefix: 'test-results'}
+          );
+          jasmine.getEnv().addReporter(jUnitXmlReporter);
+        }
+      });
+
+
+
+  }
 }
 ;
 //protractor protractor.conf.js --suite homepage
